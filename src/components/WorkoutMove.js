7087e6163fd52fa34114b7c626ccbbf6738cd203
Picao Baby <lioncubs11@gmail.com>
2021-06-30 00:14:11 -0400

    use non-portal version for draggable

--- src/components/WorkoutMove.js
+++ src/components/WorkoutMove.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {Draggable} from 'react-beautiful-dnd'
 
 function WorkoutMove({item, indexItem}) {
-
     return (
       <Draggable draggableId={`${item.id}`} index={indexItem}>
-      {(provided, snapshot) => {
-        const isDrag = snapshot.isDragging
-
-        const xxx = (
-          <a href="/" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-            <img className="workout_thumb" src="./images/1.jpg" alt="" />
-          </a>
-        )
-
-        return !isDrag ? xxx : ReactDOM.createPortal(xxx, document.getElementById('portal'))
-      }}
+      {(provided, snapshot) => (
+        <a href="#" className="drag" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} isDragging={snapshot.isDragging}>
+          <img className="workout_thumb" src="./images/1.jpg" alt="" />
+        </a>
+        )}
       </Draggable>
     )
 }
